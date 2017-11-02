fb0beaab8a9fc1a98b86896c7232d4a87948c5db
Liam English-Birge <liamebirge@gmail.com>
2017-11-01 20:37:38 -0700

    links to images finalized, style updated

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-function gotoNew(url){
-
-}
 function gotoSame(url){
-	
+  	var win = window.open(url, '_self');
+    win.focus();
+}
+
+function gotoNew(url){
+    var win = window.open(url, '_blank');
+    win.focus();
 }
