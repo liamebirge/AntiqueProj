b1c87f499b2d8846975320a05273de07376d9137
Liam English-Birge <liamebirge@gmail.com>
2017-11-06 12:11:00 -0800

    adding reverse image of antiques and restructuring file system

--- script.js
+++ script.js
@@ -7,3 +7,15 @@ function gotoNew(url){
     var win = window.open(url, '_blank');
     win.focus();
 }
+
+function flipImg(antiqueNum){
+	document.getElementById("antiqueImg");
+	if (document.getElementById("antiqueImg").src == "https://liamebirge.github.io/AntiqueProj/Pictures/antique" + antiqueNum + ".jpg") 
+        {
+            document.getElementById("antiqueImg").src = "https://liamebirge.github.io/AntiqueProj/Pictures/antique" + antiqueNum + "R.jpg";
+        }
+        else 
+        {
+            document.getElementById("antiqueImg").src = "https://liamebirge.github.io/AntiqueProj/Pictures/antique" + antiqueNum + ".jpg";
+        }
+}
