021bd47a80cd41c6c44e761d15ad9aeeb2e670ec
Liam English-Birge <liamebirge@gmail.com>
2017-11-06 12:24:44 -0800

    correcting syntax errors

--- script.js
+++ script.js
@@ -9,10 +9,10 @@ function gotoNew(url){
 }
 
 function flipImg(antiqueNum){
-	document.getElementById("antiqueImg");
-	if (document.getElementByClassName("antiqueImg").src == "https://liamebirge.github.io/AntiqueProj/Pictures/antique" + antiqueNum + ".jpg") {
-		document.getElementByClassName("antiqueImg").src = "https://liamebirge.github.io/AntiqueProj/Pictures/antique" + antiqueNum + "R.jpg";
+	var image = document.getElementById("antiqueImg");
+	if (image.src == "https://liamebirge.github.io/AntiqueProj/Pictures/antique" + antiqueNum + ".jpg") {
+		image.src = "https://liamebirge.github.io/AntiqueProj/Pictures/antique" + antiqueNum + "R.jpg";
 	}else {
-		document.getElementByClassName("antiqueImg").src = "https://liamebirge.github.io/AntiqueProj/Pictures/antique" + antiqueNum + ".jpg";
+		image.src = "https://liamebirge.github.io/AntiqueProj/Pictures/antique" + antiqueNum + ".jpg";
 	}
 }
